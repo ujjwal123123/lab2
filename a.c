f51f6e6e4f39f00c40faa92afe1a6127ad459fd9
Ujjwal Goel <ujjwal234goel@gmail.com>
2021-01-26 18:34:30 +0530

    Handle SIGINT and file redirection

--- a.c
+++ a.c
@@ -1,5 +1,8 @@
 #include <ctype.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <setjmp.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +11,15 @@
 
 #define COLOR "\x1B[31m"
 #define RESET "\x1B[0m"
+
+jmp_buf sigterm_buffer;
+
+// do not do anything on ctrl-c
+void sigint_handler() {
+    // with help from
+    // https://stackoverflow.com/questions/57480852/how-to-handle-control-c-signal-while-designing-a-shell
+    printf("\n");
+}
 
 // Returns array of null-terminated strings
 char **parse_command_with_spaces(char *command) {
@@ -76,8 +88,7 @@ int parse_file_redirection(int fd[], char *command) {
             while (*(itr + 1) == ' ') {
                 itr++;
             }
-            out_file_name = strtok(itr + 1, "<");
-            strstrip(out_file_name);
+            out_file_name = itr + 1;
         }
         else if (*itr == '<') {
             *itr = '\0';
@@ -85,12 +96,11 @@ int parse_file_redirection(int fd[], char *command) {
                 itr++;
             }
             in_file_name = itr + 1;
-            in_file_name = strtok(itr + 1, ">");
-            strstrip(in_file_name);
         }
     }
 
     if (in_file_name) {
+        strstrip(in_file_name);
         in_fd = open(in_file_name, O_RDONLY);
         if (in_fd < 0) {
             fprintf(stderr, "Cannot open file\n");
@@ -98,6 +108,7 @@ int parse_file_redirection(int fd[], char *command) {
         }
     }
     if (out_file_name) {
+        strstrip(out_file_name);
         out_fd = creat(out_file_name, 0644); // this is rw-r--r--
         if (out_fd < 0) {
             fprintf(stderr, "Cannot create file %d\n", __LINE__);
@@ -110,12 +121,17 @@ int parse_file_redirection(int fd[], char *command) {
     return 0;
 }
 
-// Retuns 0 if a command was executed. The command must not contain a
-// pipe or a semi-colon.
+// Retuns 0 if a command was executed. The command must not
+// contain a pipe or a semi-colon.
 int exec_single_command(char *command) {
+    // TODO: output in a file is not correct
+
     int fd[2]; // in, out
 
-    parse_file_redirection(fd, command);
+    if (parse_file_redirection(fd, command) != 0) {
+        fprintf(stderr, "Error: Could not parse file redirections\n");
+        return -1;
+    };
 
     char **args = parse_command_with_spaces(command);
 
@@ -128,6 +144,15 @@ int exec_single_command(char *command) {
     int pid = fork();
 
     if (pid == 0) {
+        if (fd[0] != -1) {
+            dup2(fd[0], STDIN_FILENO);
+            fsync(fd[0]);
+        }
+        if (fd[1] != -1) {
+            dup2(fd[1], STDOUT_FILENO);
+            fsync(fd[1]);
+        }
+
         int status = execvp(args[0], args);
         if (status == -1) {
             fprintf(stderr, "Error: Could not execute the command\n");
@@ -135,9 +160,9 @@ int exec_single_command(char *command) {
         return 0;
     }
     else {
-        free(args);
-        int status;
+        int status = 0;
         wait(&status);
+        free(args);
         return WEXITSTATUS(status);
     }
 }
@@ -180,7 +205,7 @@ int exec_command_with_pipe(char *command) {
 }
 
 // Execute a command which may consist of a number of commands
-// separated by `;`
+// separated by `;`q
 int exec_multi_command(char *multi_command) {
     char **parsed_commands_without_semicolon =
         (char **)malloc(sizeof(char *) * 200);
@@ -199,16 +224,25 @@ int exec_multi_command(char *multi_command) {
     return 0;
 }
 
+void get_commands() {}
+
 int main() {
     char *current_dir_buf = (char *)malloc(sizeof(char) * 200);
 
     char *command = (char *)malloc(sizeof(char) * 500);
 
+    sigaction(SIGINT,
+              &(struct sigaction){.sa_handler = sigint_handler},
+              NULL);
+
     while (1) {
         printf(COLOR "%s $ " RESET, getcwd(current_dir_buf, 200));
         int status = scanf(" %[^\n]s", command);
 
-        if (status == -1) {
+        if (status == EOF) {
+            if (errno == EINTR) {
+                continue; // read operation interrupted by signal
+            }
             // handle Ctrl-D
             exec_multi_command("exit");
         }
